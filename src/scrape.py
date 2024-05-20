86aa1b1dd2f6f0cd937f9377a27026c57f4ff3a5
Felipe Duarte <feduarte.dev@gmail.com>
2024-05-20 10:34:43 -0300

    exercicio pronto

--- src/scrape.py
+++ src/scrape.py
@@ -1,2 +1,14 @@
+import requests
+from parsel import Selector
+
+
 def scrape(url: str) -> str:
-    raise NotImplementedError
+    response = requests.get(url)
+    teste = Selector(text=response.text)
+    title = teste.css(".product_main h1::text").get()
+    price = teste.css(".price_color::text").re_first(r"\d+\.\d{2}")
+    description = teste.css("#product_description ~ p::text").get()
+    description = description[: -len("...more")]
+    image = teste.css(".thumbnail img::attr(src)").get()
+
+    return f"{title}, {price}, {description}, {'http://books.toscrape.com/catalogue/'+image}"
